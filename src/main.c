c2d9b0d7b4e4760fe140589063817ce973c1e196
tan <tanaytekin@gmail.com>
2021-02-28 19:48:32 +0300

    win32 test

--- src/main.c
+++ src/main.c
@@ -1,152 +1,198 @@
+
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <cglm/cglm.h>
-
-#include "log.h"
-
-/* temp */
-#include "shader.h"
-#include "batch.h"
-#include <time.h>
 
 
-#define SCR_WIDTH 1280
-#define SCR_HEIGHT 720
-#define W_TITLE "Gazoz"
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
-/* TODO : Do a proper setup */
-void GLAPIENTRY gzz_opengl_debug_callback (GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
-                                           const GLchar *message, const void *userParam)
-{
-    if(type == GL_DEBUG_TYPE_ERROR)
-    {
-        GZZ_LOG_ERROR("OpenGL error | severity : 0x%x\n%s", severity, message);
-    }
-    else
-    {
-        /* TODO : For now */
-        return;
-        switch (severity)
-        {
-            case GL_DEBUG_SEVERITY_HIGH:
-            case GL_DEBUG_SEVERITY_MEDIUM:
-                GZZ_LOG_WARNING("OpenGL warning | severity : 0x%x\n%s", severity, message);
-                break;
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-            case GL_DEBUG_SEVERITY_LOW:
-            case GL_DEBUG_SEVERITY_NOTIFICATION:
-                GZZ_LOG_INFO("OpenGL info | severity : 0x%x\n%s", severity, message);
-                break;
-        }
-    }
-
-}
-
-static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-
-typedef struct vertex
-{
-    vec2 position;
-    vec4 color;
-}vertex;
-
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
 
 int main()
 {
-    /* Init GLFW */
-    if(glfwInit() != GLFW_TRUE)
-    {
-        GZZ_LOG_FATAL("Failed to initialize GLFW.");
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, W_TITLE, NULL, NULL);
-
-    if(window == NULL)
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
-        GZZ_LOG_FATAL("Failed to initialize window.");
-    }
 
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
-
-    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
-        GZZ_LOG_FATAL("Failed to initialize glad.");
-    }
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    glfwSwapInterval(0);
-
-    /* Enable OpenGL debugging */
-    glEnable(GL_DEBUG_OUTPUT);
-    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-    glDebugMessageCallback(gzz_opengl_debug_callback, NULL);
-
-
-
-
-    u32 shader = gzz_shader_create("../shaders/vertex.glsl", "../shaders/fragment.glsl");
-
-    mat4 projection;
-    glm_ortho(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f, projection);
-
-    gzz_shader_use(shader);
-    gzz_shader_set_mat4(shader, "projection", projection);
-
-
-#define QUAD_COUNT 100000
-    gzz_batch *batch = gzz_batch_create(QUAD_COUNT);
-
-    srand(time(NULL));
-
-
-    f32 delta_time = 0.0f;
-    f32 last_time = 0.0f;
-    /* Main Loop */
-    while(!glfwWindowShouldClose(window))
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        f32 current_time = (f32)glfwGetTime();
-        delta_time = current_time -last_time;
-        last_time = current_time;
 
-        fprintf(stderr, "%f\n", 1.0f/delta_time);
+        return -1;
+    }
 
 
-        glfwSwapBuffers(window);
-        glClearColor(0.0f, 0.0f, 0.0f,1.0f);
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        for(u32 i=0; i<QUAD_COUNT; i++)
-        {
-            f32 x = (f32)rand()/RAND_MAX * 16.0f;
-            f32 y = (f32)rand()/RAND_MAX * 9.0f;
-
-            f32 r = (f32)rand()/RAND_MAX;
-            f32 g = (f32)rand()/RAND_MAX;
-            f32 b = (f32)rand()/RAND_MAX;
-
-
-            gzz_batch_add_rectangle(batch,x, y, 0.1f, 0.1f, r, g, b, 1.0f);
-        }
-        gzz_batch_draw(batch);
-
-
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    gzz_shader_destroy(shader);
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
-    glfwDestroyWindow(window);
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
-
     return 0;
 }
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
